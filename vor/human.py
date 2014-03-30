452e69fd3d56d1cf0a21f91aa0f6bfddc71683d4
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-03-30 19:15:40 -0400

    FIRST

---
+++ vor/human.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import RandomNameBase
+
+
+class RandomHuman(RandomNameBase):
+    def __init__(self):
+        pass
+
+    def get_first_name(self):
+        pass
+
+    def get_last_name(self):
+        pass
+
+    def get_full_name(self):
+        pass
+
+
+class RandomMale(RandomHuman):
+    def __init__(self):
+        pass
+
+    def get_first_name(self):
+        pass
+
+
+class RandomFemale(RandomHuman):
+    def __init__(self):
+        pass
+
+    def get_first_name(self):
+        pass
+
+
+# vim: filetype=python
